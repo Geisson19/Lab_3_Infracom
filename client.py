81483b61738a1fe4ed37d1c00afa7680f6a1c52c
Geisson <60269749+Geisson19@users.noreply.github.com>
2021-10-04 23:27:09 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,33 +1,54 @@
+import os
 import socket
-from threading import Thread
-import threading
+from hashlib import sha256
 
-# Informacion del servidor
-ServerSocket = socket.socket()
-# host_ip = socket.gethostbyname(socket.gethostname)
+host_ip = socket.gethostbyname(socket.gethostname())
 port = 1233
 
 
-class ClienteThread(Thread):
+def generarHash(path):
+    hash = sha256()
+    with open(path, 'rb') as f:
+        while True:
+            bloque = f.read(4096)
+            if not bloque:
+                break
+            hash.update(bloque)
+    f.close()
+    return hash.hexdigest()
 
-    def __init__(self, ip, port, clientsock):
-        Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.clientsock = clientsock
-        print('Nuevo thread @{p} para {add}'.format(p=self.port, add = self.ip))
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    def run(self):
-        print ("Connection from {add}: {p}".format(add = self.ip, p = self.port))
+    tam_bloque = 4096
 
-        self.clientsock.send("\nWelcome to the server\n\n")
+    tup = (host_ip, port)
+    client.connect(tup)
 
-        data = "dummydata"
+    bienvenida = client.recv(tam_bloque)
+    print(bienvenida.decode())
 
-        while len(data):
-            data = self.clientsock.recv(2048)
-            print("Cliente envió : "+data)
-            self.clientsock.send("Usted me envió : "+data)
+    hash_servidor = client.recv(tam_bloque).decode()
 
-        print( "Cliente desconectado...")
+    cConexiones = client.recv(tam_bloque).decode()
+    print(f"Cantidad de conexiones: {cConexiones}")
 
+    path = client.recv(tam_bloque).decode()
+    fname = path.split("/")[1]
+
+    archivo_recibido = client.recv(tam_bloque)
+    path = 'archivosRecibidos/'
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    with open(path + fname, "wb") as f:
+        f.write(archivo_recibido)
+
+    hash_cliente = generarHash(path + fname)
+    if hash_servidor == hash_cliente:
+        client.send("Los hashes coinciden :)".encode())
+    else:
+        client.send("Los hashes no coinciden :(".encode())
+
+if __name__ == "__main__":
+    main()
