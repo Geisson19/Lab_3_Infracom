b4d5fefa2968e8c1cdea2d00f7e2a3361a9c6bdb
Geisson <60269749+Geisson19@users.noreply.github.com>
2021-10-04 23:27:39 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,67 +1,73 @@
 import socket
 import os
 from threading import Thread
-import threading
-from client import ClienteThread
 from hashlib import sha256
 import time 
 from datetime import datetime
-# struct, hashlib, time
-# Informacion del servidor
-port = 1233
-host_ip = socket.gethostbyname(socket.gethostname())
 
 threads = []
 
 def main():
-    print('[*] Iniciando servidor ...')
+    # Variables de entorno
+    host_ip = socket.gethostbyname(socket.gethostname())
+    port = 1233
+
+    print("[*] Iniciando servidor ...")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    arch = input('[/] Seleccione el tipo de archivo a enviar: (1 o 2)')
-    print('Archivo tipo 1: 100 MB')
-    print('Archivo tipo 2: 250 MB')
-    path = 'archivosServidor/'
-    if(arch == '1'):
-        path += 'archivo1.txt'
+    print("Archivo tipo 1: 100 MB")
+    print("Archivo tipo 2: 250 MB")
+    arch = input("[/] Seleccione el tipo de archivo a enviar: (1 o 2)\n")
+    
+    path = "archivosServidor/"
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    if(arch == "1"):
+        path += "archivo1.txt"
         with open(path, "wb") as f:
             f.seek(100*1024**2)
-            f.write("\0")
+            f.write("Infracom".encode())
     else:
-        path += 'archivo2.txt'
+        path += "archivo2.txt"
         with open(path, "wb") as f:
             f.seek(250*1024**2)
-            f.write("\0") 
+            f.write("Infracom".encode()) 
 
     # Numero de Threads para las conexiones concurrentes
-    nThreads = int(input('Ingrese el número de threads: '))
+    nThreads = int(input("Ingrese el número de threads: "))
     tup = (host_ip, port)
     server.bind(tup)
     server.listen()
-    print('[*] El servidor está escuchando en el puerto {p}.'.format(p=port))
+    print("[*] El servidor está escuchando en el puerto {p}.".format(p=port))
 
     while True:
         connection, (ip, port) = server.accept()
-        thread = threading.Thread(target = operate, args = (connection, ip, port, path, nThreads))
+        thread = Thread(target = operate, args = (connection, ip, port, path, nThreads))
         thread.start()
         threads.append([ip, port])
         
 def operate(connection, ip, port, path, nThreads):
-    print('Conexión establecida. {add} @ {p}'.format(add=ip, p = port))
-    connection.send('Bienvenido, {}!'.format(ip))
+    print(f"Conexión establecida. {ip} @ {port}".encode())
+    connection.send(f"Bienvenido, {ip}!".encode())
     fhash = generarHash(path)
     fsize = os.path.getsize(path)
-    connection.sendall('Hash: {}'.format(fhash))
+    connection.send(f"Hash: {fhash}".encode())
+    connection.send(f"Cantidad de conexiones: {nThreads}".encode())
+    connection.send(f"Archivo: {path}".encode())
+
     tiempo1 = time.time()
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         file = f.read()
-    connection.send(file)
+    connection.send(file.encode())
+
     tiempo2 = time.time()
     tiempo = tiempo2 - tiempo1
-    
+    generarLog(path, ip, port, fsize, tiempo)
 
 def generarHash(path):
     hash = sha256()
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         while True:
             bloque = f.read(4096)
             if not bloque:
@@ -71,29 +77,25 @@ def generarHash(path):
     return hash.hexdigest()
 
 
-def generarLog(path, ip, port, tiempo):
-    fechaActual = datetime.now()
-    log = '{actualAnho}-{actualMes}-{actualDia}-{actualHora}-{actualMinuto}-{actualSegundo}-log.txt'.format(actualAnho = fechaActual.year,
-                                                                                                            actualMes = fechaActual.month, 
-                                                                                                            actualDia = fechaActual.day,
-                                                                                                            actualHora = fechaActual.hour,
-                                                                                                            actualMinuto = fechaActual.minute,
-                                                                                                            actualSegundo = fechaActual.second )
-    fileLog = open('logs/{}'.format(log), 'x')
+def generarLog(path, ip, port, fsize, tiempo):
 
-    fileLog.write('Log {}\n'.format(fechaActual))
-    fileLog.write('Nombre del archivo: {}\n'.format(path.split('/')[1]))
-    fileLog.write('Tamaño del archivo: {}'.format(os.path.getsize(path)))
-    fileLog.write('')
-    fileLog.write('####################################################\n')
-    fileLog.write('* ip: {add}\n* port: {p}\n* time: {t}\n'.format(add=ip, p=port, t = tiempo*1000))
-    fileLog.write('')
+    if not os.path.isdir("./logs"):
+        os.mkdir("./logs")
+
+    fActual = datetime.now()
+    log = f"{fActual.year}-{fActual.month}-{fActual.day}-{fActual.hour}-{fActual.minute}-{fActual.second}-log.txt"
+    
+    fileLog = open(f"logs/{log}", "x")
+
+    fileLog.write("Log {}\n".format(fActual))
+    fileLog.write("Nombre del archivo: {}\n".format(path.split("/")[1]))
+    fileLog.write("Tamaño del archivo: {}".format(fsize))
+    fileLog.write("\n")
+    fileLog.write("####################################################\n")
+    fileLog.write("* ip: {add}\n* port: {p}\n* time: {t}\n".format(add=ip, p=port, t = tiempo*1000))
+    fileLog.write("")
 
     fileLog.close()
 
-
-
-    
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
